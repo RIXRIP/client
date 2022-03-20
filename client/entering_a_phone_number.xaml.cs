6c21e57a4b8e0ad5a3a07f29366e2c9bf37fdf1e
RIXRIP <RIXRIP@LAPTOP-PV6UBHT3>
2022-03-20 16:54:11 +0300

    Добавьте файлы проекта.

---
+++ client/entering_a_phone_number.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client
+{
+    /// <summary>
+    /// Логика взаимодействия для entering_a_phone_number.xaml
+    /// </summary>
+    public partial class entering_a_phone_number : Page
+    {
+        public entering_a_phone_number()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void send_sms_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ client/main_page.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client
+{
+    /// <summary>
+    /// Логика взаимодействия для main_page.xaml
+    /// </summary>
+    public partial class main_page : Page
+    {
+        public main_page()
+        {
+            InitializeComponent();
+        }
+
+        private void friends_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new friend_page());
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
